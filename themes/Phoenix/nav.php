c8e54be39efc1e7a9a3a7835a8ecefeb9ef3e3cc
bfreeman <bfreeman>
2005-12-11 04:45:43 +0000

    changing CSS for fly-out menus

--- themes/Phoenix/nav.php
+++ themes/Phoenix/nav.php
@@ -13,81 +13,35 @@
 <?php
 			}
 			$level++;
-			?><!-- += <?= $level ?> -->
-<?php
 		} elseif( $level > $newlevel ) {
 			while( $level > $newlevel ) {
-				?></li></ul>
-				<?php
+				?></li></ul><?php
 				$level--;
 			}
-			?><!-- -= <?= $level ?> -->
+			?> 
+<?= substr("                  ", 0, $level * 2) ?></li>
 <?php
 		} else {
-			?></li><?php
+			?></li>
+<?php
 		}
 		if($Link->getTarget() == "__SEPARATOR") {
-			?><li class="separator"><a class="separator" href="<?= $Link->getURL() ?>"><?= $Link->getText() ?> &#160;&#160;<img src="/eclipse.org-common/themes/Phoenix/images/leftnav_bullet_down.gif" border="0" alt="" /></a>
-<?php
+			?>
+<?= substr("                  ", 0, $level * 2) ?><li class="separator"><a class="separator" href="<?= $Link->getURL() ?>"><?= $Link->getText() ?> &#160;&#160;<img src="/eclipse.org-common/themes/Phoenix/images/leftnav_bullet_down.gif" border="0" alt="" /></a><?php
 		} else {
-			?><li><a href="<?= $Link->getURL() ?>"><?= $Link->getText() ?></a>
-<?php
+			?>
+<?= substr("                  ", 0, $level * 2) ?><li><a href="<?= $Link->getURL() ?>"><?= $Link->getText() ?></a><?php
 		}
 	}
 	for($i = $level; $i > 1; $i-- ) {
-		?></li></ul>
-<?php
+		?></li></ul><?php
 	}
-/*
-		$liClass = "";
-		$aClass = "";
-		$aClassSelected = "";
-		$Link = $Nav->getLinkAt($i);
-		
-
-		if($Link->getLevel() == 2 || $Link->getLevel() == 3) {
-			$aClass = "class=\"level" . $Link->getLevel();
-			if($Link->getURL() && strpos($_SERVER['SCRIPT_FILENAME'], $Link->getURL())) {
-				$aClass .= "selected";
-			}
-			
-			$aClass .= "\"";
-		}
-		
-		# Selected tab
-		
-		if($Link->getTarget() == "__SEPARATOR") {
-			$liClass = "class=\"separator\"";
-			$aClass = "class=\"separator\"";
-			$Link->setTarget("");
-			
-			$Link->setText($Link->getText() . "&#160;&#160;<img src=\"/eclipse.org-common/themes/Phoenix/images/leftnav_bullet_down.gif\" border=\"0\" alt=\"\" />");
-		}
-		$target = "";
-		if($Link->getTarget() != "" && $Link->getTarget() != "_self") {
-			$target = "target=\"" . $Link->getTarget() . "\"";
-		}
-
-		if( $Link->getLevel() == 2 ) {
-?    ?php
-		}
-		if( $Link->getLevel() == 3 ) {
-?    ?php
-		}
-        if( $Link->getURL() ) {
-?    <li <?= $liClass ?>><a <?= $aClass ?> href="<?= $Link->getURL() ?>" <?= $target ?>><?= $Link->getText() ?></a></li>
-?php
-        } else {
-?    <li <?= $liClass ?>><span <?= $aClass ?>><?= $Link->getText() ?></span></li>
-?php
-        }		
-	}	
-        */
-?>
-		<li style="background-image: url(/eclipse.org-common/themes/Phoenix/images/leftnav_fade.jpg); background-repeat: repeat-x; border-style: none;">
+	?> 
+  </li>
+  <li style="background-image: url(/eclipse.org-common/themes/Phoenix/images/leftnav_fade.jpg); background-repeat: repeat-x; border-style: none;">
 			<br /><br /><br /><br /><br />
-		</li>
-	</ul>
+  </li>
+</ul>
 <? /*
 	<br />
 	<div class="sideitem">
