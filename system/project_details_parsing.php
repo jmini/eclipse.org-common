a4cd6e856a21167bebdff76d46508cc4ef3692cb
mward <mward>
2005-12-14 20:16:20 +0000

    *** empty log message ***

--- system/project_details_parsing.php
+++ system/project_details_parsing.php
@@ -21,7 +21,7 @@
 ****************************************/
 function NewsParse( $name, &$html, $show_desc) {
   //build up the name of hte file on the local filesystem
-  $group_file = $_SERVER['DOCUMENT_ROOT'] . "/" . $name . "/" . "newsgroup";
+  $group_file = $_SERVER['DOCUMENT_ROOT'] . "/" . $name . "/project-info/" . "newsgroup";
   if( file_exists($group_file) ) {
   	//get the contents
     $contents = file_get_contents($group_file);
