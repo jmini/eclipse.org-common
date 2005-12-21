5d3f1e18013c5506b30cf0e8e6ddac545afbcad4
mward <mward>
2005-12-21 15:58:36 +0000

    *** empty log message ***

--- system/project_details_parsing.php
+++ system/project_details_parsing.php
@@ -10,6 +10,29 @@
  *
 *********************************************************/
 
+/*****************************
+ * Name: getfile
+ * Function: given a project id builds a location path
+ * I/O: the project id and the name of the file. returns the path
+ * 
+ * By: M. Ward
+ * Date: Dec 21/05
+*****************************/
+function GetFile( $name, $filename ) {
+ //breakup the name
+  $position = strrpos($name,'.');
+  if( $position === FALSE) {
+	$localname = $name; 
+  } else { 
+    $localname = substr($name, $position+1 );
+  }
+  //build up the name of hte file on the local filesystem
+  $group_file = $_SERVER['DOCUMENT_ROOT'] . "/" . $localname . "/project-info/" . $filename;
+  
+  return $group_file;
+	
+} 
+
 /***************************************
  * Name: NewsParse
  * function: Parses the news file and build the project specific html
@@ -20,15 +43,7 @@
  * Date: Dec 13/05
 ****************************************/
 function NewsParse( $name, &$html, $show_desc) {
-  //breakup the name
-  $position = strrpos($name,'.');
-  if( $position === FALSE) {
-	$localname = $name; 
-  } else { 
-    $localname = substr($name, $position+1 );
-  }
-  //build up the name of hte file on the local filesystem
-  $group_file = $_SERVER['DOCUMENT_ROOT'] . "/" . $localname . "/project-info/" . "newsgroup";
+  $group_file = GetFile( $name, "newsgroup" );
   if( file_exists($group_file) ) {
   	//get the contents
     $contents = file_get_contents($group_file);
@@ -49,5 +64,38 @@ function NewsParse( $name, &$html, $show_desc) {
 	}
   }          
 }
+
+/***************************************
+ * Name: MailParse
+ * function: Parses the mail file and build the project specific html
+ * I/O: takes the name of the project(getProjectID()) and a pointer to the html output stream, as
+ * well as a bool to turn off the descriptive text
+ * 
+ * By: M. Ward
+ * Date: Dec 21/05
+****************************************/
+function MailParse( $name, &$html, $show_desc) {
+  $group_file = GetFile( $name, "maillist");
+  if( file_exists($group_file) ) {
+  	//get the contents
+    $contents = file_get_contents($group_file);
+    //now break them down        
+    $array = explode("::",$contents);
+
+    $group_count = count($array);
+    for ( $loop = 1; $loop < $group_count; $loop+=2) {
+      $mail_name = $array[$loop];
+      $mail_html = "<a href=\"http://dev.eclipse.org/mailman/listinfo/" . $mail_name . "\""  . ">" . $mail_name . "</a>";
+	  $mailarch_html = "<a href=\"http://dev.eclipse.org/mhonarc/lists/" . $mail_name . "/maillist.html\""  . "><img src='images/save_edit.gif' alt='Archive' /></a>";
+	  $description = $array[$loop+1];
+	  if( $show_desc == TRUE )
+	    $html .= "<blockquote><p>$mail_html $mailarch_html </p><blockquote><p> $description </p></blockquote></blockquote>";
+	  else
+	  $html .= "$mail_html $mailarch_html";
+	}
+  }          
+}
+
+
 
 ?>
