1c3cbebca7b4a128faf9e51c17da7f2c4f2ed93e
mward <mward>
2005-12-21 18:49:53 +0000

    *** empty log message ***

--- system/project_details_parsing.php
+++ system/project_details_parsing.php
@@ -92,7 +92,7 @@ function MailParse( $name, &$html, $show_desc) {
 	    $html .= "<blockquote><p>$mail_html $mailarch_html </p><blockquote><p> $description </p></blockquote></blockquote>";
 	  else
 	  $html .= "$mail_html $mailarch_html";*/
-	  $html .= "<p><a onclick=\"switchMenu('$mail_name');\" title=\"desc\">+</a> <blockquote>$mail_html $mailarch_html </p><blockquote> <div id=\"$mail_name\"> <p> $description </p></div>";
+	  $html .= "<blockquote> <a onclick=\"switchMenu('$mail_name');\" title=\"desc\">Desc</a> $mail_html $mailarch_html </blockquote> <div id=\"$mail_name\"> <p> $description </p></div>";
 	}
   }          
 }
