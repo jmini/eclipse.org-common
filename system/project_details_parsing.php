28fd00a286074d2854834eb10ba4a8dff70f347c
mward <mward>
2006-01-03 16:21:14 +0000

    *** empty log message ***

--- system/project_details_parsing.php
+++ system/project_details_parsing.php
@@ -22,7 +22,7 @@ function GetFile( $name, $filename, $docroot ) {
 
   if( substr($name,-1,1) != "/" )
     $name .= "/";
-  $name = str_replace("index.html","/", $name);
+  $name = str_replace("index.html","", $name);
   $name = str_replace("http://www.eclipse.org/", $docroot . "/", $name);
   $localname = str_replace("http://eclipse.org/", $docroot . "/", $name);
   //build up the name of hte file on the local filesystem
