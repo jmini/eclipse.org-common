a7187c6e3b167160856e0bc168368b28c89d2f4d
mward <mward>
2006-01-05 16:36:09 +0000

    *** empty log message ***

--- system/project_details_parsing.php
+++ system/project_details_parsing.php
@@ -62,11 +62,11 @@ function NewsParse( $name, &$html, $id ) {
   $group_count = count($array);
   for ( $loop = 1; $loop < $group_count; $loop+=2) {
     $news_name = $array[$loop];
-    $news_html = "<a href=\"news://news.eclipse.org/" . $news_name . "\" ><img src='images/file_obj.gif' alt='News server' title=\"News server\"/></a>";
+    $news_html = "<a href=\"news://news.eclipse.org/" . $news_name . "\" alt='News server' title=\"News server\"/>$news_name</a>";
     $webnews_html = "<a href=\"http://www.eclipse.org/newsportal/thread.php?group=" . $news_name . "\""  . "><img src='images/discovery.gif' alt='Web interface' title=\"Web interface\" /></a>";
-	$newsarch_html = "<a href=\"http://dev.eclipse.org/newslists/news." . $news_name . "/maillist.html\""  . "><img src='images/save_edit.gif' alt='Archive' title=\"Archive\" /></a>";
+	$newsarch_html = "<a href=\"http://dev.eclipse.org/newslists/news." . $news_name . "/maillist.html\""  . "><img src='images/archive.gif' alt='Archive' title=\"Archive\" /></a>";
 	$description = $array[$loop+1];
-	$html .= "<blockquote> <a href=\"javascript:switchMenu('$news_name.$id');\" title=\"Description\" alt='Description' >$news_name</a>  $news_html $webnews_html $newsarch_html </blockquote> <div id=\"$news_name.$id\" class=\"switchcontent\"> <p> $description </p></div>";
+	$html .= "<blockquote> <a href=\"javascript:switchMenu('$news_name.$id');\" title=\"Description\"><img src='images/plus.gif' alt='Description' title=\"Description\"></a>  $news_html $webnews_html $newsarch_html </blockquote> <div id=\"$news_name.$id\" class=\"switchcontent\"> <p> $description </p></div>";
   }          
 }
 
