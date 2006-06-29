4be94661248cf52196097225dd8283759ac56aa5
ngervais <ngervais>
2006-06-29 14:48:38 +0000

    *** empty log message ***

--- themes/Phoenix/switcher.php
+++
@@ -1,7 +0,0 @@
-<?php
-$set = $_GET['set'];
-$domain = $_SERVER['HTTP_HOST'];
-setcookie ('sitestyle', $set, time()+31536000, '/', $domain, '0');
-$HTTP_REFERER = $_SERVER['HTTP_REFERER'];
-header("Location: $HTTP_REFERER");
-?>
