67ef4be8796bd19deb9740d2704db368629b843a
droy <droy>
2006-08-02 20:40:07 +0000

    [144911] project-specific branding

--- themes/Lazarus/header.php
+++ themes/Lazarus/header.php
@@ -2,32 +2,21 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<title><?= $pageTitle ?></title>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 	<meta name="author" content="<?= $pageAuthor ?>" />
 	<meta name="keywords" content="<?= $pageKeywords ?>" />
-	<link rel="stylesheet" type="text/css" href="/eclipse.org-common/themes/Phoenix/css/visual.css" media="screen" />
-	<link rel="stylesheet" type="text/css" href="/eclipse.org-common/themes/Phoenix/css/layout.css" media="screen" />
+	<link rel="stylesheet" type="text/css" href="/eclipse.org-common/themes/Lazarus/css/visual.css" media="screen" />
+	<link rel="stylesheet" type="text/css" href="/eclipse.org-common/themes/Lazarus/css/layout.css" media="screen" />
 	<link rel="stylesheet" type="text/css" href="/eclipse.org-common/themes/Phoenix/css/print.css" media="print" />
 	<?php if( isset($extraHtmlHeaders) ) echo $extraHtmlHeaders; ?>
 </head>
+
 <body>
 <div id="header">
-	<a href="/"><img src="/eclipse.org-common/themes/Phoenix/images/header_logo.gif" width="163" height="68" border="0" alt="Eclipse Logo" class="logo" /></a>
-	<div id="searchbar">
-		<img src="/eclipse.org-common/themes/Phoenix/images/searchbar_transition.gif" width="92" height="26" class="transition" alt="" />
-		<img src="/eclipse.org-common/themes/Phoenix/images/searchbar_header.gif" width="64" height="17" class="header" alt="Search" />
-		<form method="get" action="/search/search.cgi">
-			<input type="hidden" name="t" value="All" />
-			<input type="hidden" name="t" value="Doc" />
-			<input type="hidden" name="t" value="Downloads" />
-			<input type="hidden" name="t" value="Wiki" />
-			<input type="hidden" name="wf" value="574a74" />
-			<input type="text" name="q" value="" />
-			<input type="image" class="button" src="/eclipse.org-common/themes/Phoenix/images/searchbar_submit.gif" alt="Submit" onclick="this.submit();" />
-		</form>
-	</div>
-	<ul id="headernav">
-		<li class="first"><a href="/org/foundation/contact.php">Contact</a></li>
-		<li><a href="/legal/">Legal</a></li>
-	</ul>
+</div>
+
+<div id="headerlogo">
+
+	<a href="/"><img src="/eclipse.org-common/themes/Lazarus/images/header_logo.gif" alt="Eclipse Logo" class="logo" border="0" height="75" width="241" /></a>
+
 </div>
