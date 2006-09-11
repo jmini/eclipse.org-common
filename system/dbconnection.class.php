2358f0cc21adb12b158b65926ed67ed670f7726a
ngervais <ngervais>
2006-09-11 19:10:40 +0000

    *** empty log message ***

---
+++ system/dbconnection.class.php
@@ -0,0 +1,39 @@
+<?
+
+class DBConnection {
+		#*****************************************************************************
+        #
+        # dbconnection.class.php
+        #
+        # Author:       Denis Roy
+        # Date:         2004-08-05
+        #
+        # Description: Functions and modules related to the MySQL database connection
+        #
+        # HISTORY:  THIS IS A READ-ONLY DB CONNECTION CLASS!
+        #
+		#*****************************************************************************
+
+        var $MysqlUrl           = "localhost";
+        var $MysqlUser          = "phoenix";
+        var $MysqlPassword      = "fireB!rd";
+        var $MysqlDatabase      = "pillars";
+
+        function connect()
+        {
+                static $dbh;
+
+                $dbh = mysql_connect($this->MysqlUrl, $this->MysqlUser, $this->MysqlPassword);
+
+                if (!$dbh) {
+                        echo( "<P>Unable to connect to the database server at this time.</P>" );
+                }
+                $DbSelected = mysql_select_db($this->MysqlDatabase, $dbh);
+                return $dbh;
+        }
+
+        function disconnect() {
+                mysql_close();
+        }
+}
+?>
