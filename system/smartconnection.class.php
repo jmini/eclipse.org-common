14ae686b2cbacddc897887958a3e4d4512bc69ec
wbeaton <wbeaton>
2006-09-18 15:57:47 +0000

    Tightened up logic. Now just checks for existance of dbconnection file.

--- system/smartconnection.class.php
+++ system/smartconnection.class.php
@@ -1,11 +1,7 @@
 <?php
 
-	if ($_SERVER['SERVER_NAME'] == "www.eclipse.org")
+	if (file_exists("/home/data/httpd/eclipse-php-classes/system/dbconnection.class.php"))
 		require_once "/home/data/httpd/eclipse-php-classes/system/dbconnection.class.php";
-		
-	elseif ($_SERVER['SERVER_NAME'] == "phoenix.eclipse.org")
-		require_once "/home/data/httpd/eclipse-php-classes/system/dbconnection.class.php";
-	
 	else
 		require_once ($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/dbconnection.class.php");
 
