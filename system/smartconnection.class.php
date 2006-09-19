b186058e2468089585fa1bd18fee7a49b7363021
wbeaton <wbeaton>
2006-09-19 17:27:39 +0000

    Added generation of comments to indicate which database is being used.

--- system/smartconnection.class.php
+++ system/smartconnection.class.php
@@ -1,8 +1,11 @@
 <?php
 
-	if (file_exists("/home/data/httpd/eclipse-php-classes/system/dbconnection.class.php"))
+	if (file_exists("/home/data/httpd/eclipse-php-classes/system/dbconnection.class.php")) {
+		echo "<!-- Using server database -->";
 		require_once "/home/data/httpd/eclipse-php-classes/system/dbconnection.class.php";
-	else
+	} else {
+		echo "<!-- Using local database -->";
 		require_once ($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/dbconnection.class.php");
+	}
 
 ?>
