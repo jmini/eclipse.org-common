17278341071ca9056a141e230f0895b08584bb2c
wbeaton <wbeaton>
2006-09-19 17:37:00 +0000

    Updated to use the read/write database class.

--- system/smartconnection.class.php
+++ system/smartconnection.class.php
@@ -1,8 +1,8 @@
 <?php
 
-	if (file_exists("/home/data/httpd/eclipse-php-classes/system/dbconnection.class.php")) {
+	if (file_exists("/home/data/httpd/eclipse-php-classes/system/dbconnection_rw.class.php")) {
 		echo "<!-- Using server database -->";
-		require_once "/home/data/httpd/eclipse-php-classes/system/dbconnection.class.php";
+		require_once "/home/data/httpd/eclipse-php-classes/system/dbconnection_rw.class.php";
 	} else {
 		echo "<!-- Using local database -->";
 		require_once ($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/dbconnection.class.php");
