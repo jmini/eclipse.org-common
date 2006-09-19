b6f23b5c29cf7616ca498d8a19ddb83d90761bc8
wbeaton <wbeaton>
2006-09-19 17:49:50 +0000

    Added comment generation after server-based dbconnection class is loaded.

--- system/smartconnection.class.php
+++ system/smartconnection.class.php
@@ -3,6 +3,7 @@
 	if (file_exists("/home/data/httpd/eclipse-php-classes/system/dbconnection_rw.class.php")) {
 		echo "<!-- Using server database -->";
 		require_once "/home/data/httpd/eclipse-php-classes/system/dbconnection_rw.class.php";
+		echo "<!-- Database access loaded -->";
 	} else {
 		echo "<!-- Using local database -->";
 		require_once ($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/dbconnection.class.php");
