9ea431f174f79f3bd4b532e0e98fd88d75c3e60f
ngervais <ngervais>
2006-09-22 17:53:47 +0000

    Backreving to 1.1 for database implementation test.  Please rev back to 1.7 when this is no longer needed.

--- system/smartconnection.class.php
+++ system/smartconnection.class.php
@@ -1,9 +1,12 @@
 <?php
 
-	if (file_exists("/home/data/httpd/eclipse-php-classes/system/dbconnection_rw.class.php")) {
-		require_once "/home/data/httpd/eclipse-php-classes/system/dbconnection_rw.class.php";
-	} else {
+	if ($_SERVER['SERVER_NAME'] == "www.eclipse.org")
+		require_once "/home/data/httpd/eclipse-php-classes/system/dbconnection.class.php";
+		
+	elseif ($_SERVER['SERVER_NAME'] == "phoenix.eclipse.org")
 		require_once ($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/dbconnection.class.php");
-	}
+	
+	else
+		require_once ($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/dbconnection.class.php");
 
 ?>
