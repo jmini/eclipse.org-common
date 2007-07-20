ee0ef8278ba8e4e0260928450512487190718357
ngervais <ngervais>
2007-07-20 15:13:35 +0000

    *** empty log message ***

--- themes/Phoenix/header.php
+++ themes/Phoenix/header.php
@@ -22,19 +22,57 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head><title><?= $pageTitle ?></title><meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /><meta name="author" content="<?= $pageAuthor ?>" />
 <meta name="keywords" content="<?= $pageKeywords ?>" /><link rel="stylesheet" type="text/css" href="/eclipse.org-common/themes/Phoenix/css/small.css" title="small" /><link rel="alternate stylesheet" type="text/css" href="/eclipse.org-common/themes/Phoenix/css/large.css" title="large" /><link rel="stylesheet" type="text/css" href="/eclipse.org-common/themes/Phoenix/css/visual.css" media="screen" /><link rel="stylesheet" type="text/css" href="/eclipse.org-common/themes/Phoenix/css/layout.css" media="screen" />
-<link rel="stylesheet" type="text/css" href="/eclipse.org-common/themes/Phoenix/css/print.css" media="print" /><script type="text/javascript" src="/eclipse.org-common/themes/Phoenix/styleswitcher.js"></script>
+<link rel="stylesheet" type="text/css" href="/eclipse.org-common/themes/Phoenix/css/print.css" media="print" />
+<link rel="stylesheet" type="text/css" href="/eclipse.org-common/themes/Phoenix/css/header.css" media="screen" />
+<script type="text/javascript" src="/eclipse.org-common/themes/Phoenix/styleswitcher.js"></script>
 <?php if( isset($extraHtmlHeaders) ) echo $extraHtmlHeaders; ?></head>
 <body>
 <div id="header">
-	<a href="/"><img src="/eclipse.org-common/themes/Phoenix/images/header_logo.gif" width="163" height="68" border="0" alt="Eclipse Logo" class="logo" /></a>
-	<div id="searchbar">
-		<img src="/eclipse.org-common/themes/Phoenix/images/searchbar_transition.gif" width="92" height="26" class="transition" alt="" />
-		<img src="/eclipse.org-common/themes/Phoenix/images/searchbar_header.gif" width="64" height="17" class="header" alt="Search" />
-		<form method="get" action="<?= $www_prefix ?>/search/search.cgi"><input type="hidden" name="t" value="All" /><input type="hidden" name="t" value="Downloads" /><input type="hidden" name="t" value="Live" /><input type="hidden" name="t" value="Wiki" /><input type="hidden" name="wf" value="574a74" /><input type="text" name="q" value="" /><input type="image" class="button" src="/eclipse.org-common/themes/Phoenix/images/searchbar_submit.gif" alt="Submit" onclick="this.submit();" /></form>
+	<div id="header-graphic" class="eclipse-main">
+		<h1>Eclipse</h1>
 	</div>
-	<ul id="headernav">
-		<li class="first"><a class="smallText" title="Small Text" href="#" onclick="setActiveStyleSheet('small');return false;">A</a> <a class="largeText" title="Large Text" href="#" onclick="setActiveStyleSheet('large');return false;">A</a></li>
-		<li><a href="<?= $www_prefix ?>/org/foundation/contact.php">Contact</a></li>
-		<li><a href="<?= $www_prefix ?>/legal/">Legal</a></li>
-	</ul>
+	<div id="header-global-holder"  class="eclipse-main-global">
+
+		<div id="header-global-links">
+			<ul>
+				<li><a href="#" class="first_one">Contact</a></li>
+				<li><a href="#">Legal</a></li>
+			</ul>
+		</div>
+		<div id="header-icons">
+			<a href="#"><img src="/eclipse.org-common/themes/Phoenix/images/eclipse_home_icon.png" width="28" height="28" alt="eclipse foundation button" title="Eclipse Foundation" /></a><a href="#"><img src="/eclipse.org-common/themes/Phoenix/images/planet_icon.png" width="28" height="28" alt="planet eclipse button" title="Planet Eclipse" /></a><a href="#"><img src="/eclipse.org-common/themes/Phoenix/images/plugin_icon.png" width="28" height="28" alt="eclipse plugin central button" title="Eclipse Plugin Central" /></a>
+
+		</div>
+	</div>
+	<div id="header-nav">
+		<ul>
+			<li><a href="#" class="first_one">Home</a></li>
+			<li><a href="#">Community</a></li>
+			<li><a href="#">Membership</a></li>
+
+			<li><a href="#">Committers</a></li>
+			<li><a href="#">Downloads</a></li>
+			<li><a href="#">Resources</a></li>
+			<li><a href="#">Projects</a></li>
+			<li><a href="#">About Us</a></li>
+		</ul>
+
+	</div>
+	<div id="header-utils">
+		<form action="/search/search.cgi" method="get">
+				<input type="hidden" value="All" name="t"/>
+				<input type="hidden" value="Downloads" name="t"/>
+				<input type="hidden" value="Live" name="t"/>
+				<input type="hidden" value="Wiki" name="t"/>
+				<input type="hidden" value="574a74" name="wf"/>
+				<input class="input" type="text" value="" name="q"/>
+
+				<input class="button" type="image" onclick="this.submit();" alt="Submit" title="Search" src="/eclipse.org-common/themes/Phoenix/images/search_btn.gif" width="54" height="18"/>
+		</form>
+		<ul>
+			<li class="rss_feed"><a href="http://www.eclipse.org/home/eclipsenews.rss" target="_blank"><img src="/eclipse.org-common/themes/Phoenix/images/rss_btn.gif" height="16" width="16" border="0" class="rss_icon" /></a></li>
+			<li class="text_size"><a class="smallText" title="Small Text" href="#" onclick="setActiveStyleSheet('small');return false;">A</a> <a class="largeText" title="Large Text" href="#" onclick="setActiveStyleSheet('large');return false;">A</a></li>
+		</ul>
+	</div>
+
 </div>
