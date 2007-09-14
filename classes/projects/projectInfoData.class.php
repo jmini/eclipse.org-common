bb2c838fdaa221264fc54cdfdf13c094b471c199
ngervais <ngervais>
2007-09-14 15:24:56 +0000

    Implemented Countable Extension to the class and Plural usage of __get function

--- classes/projects/projectInfoData.class.php
+++ classes/projects/projectInfoData.class.php
@@ -9,10 +9,11 @@
  * Contributors:
  *    Bjorn Freeman-Benson - Initial API
  *    Nathan Gervais - Fixed __get function to return correct values for multirow records
+ *    Karl Matthias - Implemented Countable Extension to the class. And Plural __get retreival
  *******************************************************************************/
 require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/smartconnection.class.php");
 
-class ProjectInfoData
+class ProjectInfoData implements Countable
 {
 	private $rows; // raw query results for database, ProjectID, MainKey, SubKey, Value, ProjectInfoID
 	private $mainkeys; // [main key] -> # of rows
@@ -20,11 +21,11 @@ class ProjectInfoData
 
 	function ProjectInfoData( $projectid )
 	{
-		$dbc = new DBConnection();
+		$dbc = new DBConnectionProjectInfo();
 		$dbh = $dbc->connect();
 		$result = mysql_query("
-					SELECT * FROM ProjectInfo, ProjectInfoValues 
-						WHERE ProjectID = '$projectid' 
+					SELECT * FROM ProjectInfo, ProjectInfoValues
+						WHERE ProjectID = '$projectid'
 						  AND ProjectInfo.ProjectInfoID = ProjectInfoValues.ProjectInfoID", $dbh);
 		$this->rows = array();
 		$this->mainkeys = array();
@@ -32,7 +33,7 @@ class ProjectInfoData
 		while($row = mysql_fetch_assoc($result)) {
 			$this->rows[] = $row;
 			$mainkey = $row['MainKey'];
-			if( isset($this->mainkeys[$mainkey])) 
+			if( isset($this->mainkeys[$mainkey]))
 				$this->mainkeys[$mainkey]++;
 			else
 				$this->mainkeys[$mainkey] = 1;
@@ -41,26 +42,41 @@ class ProjectInfoData
 			if( $row['SubKey'] != null )
 				$this->subkeys[$mainkey] = true;
 		}
-		
 	}
 
 	function __get( $varname )
 	{
+		$check_multiples = false;
+		if(preg_match('/s$/', $varname)) // see if we need to look for "newsgroups" instead of just "newsgroup" for example
+			$check_multiples = true;
 		foreach( $this->rows as $row ) {
 			$mainkey = $row['MainKey'];
-			if( $mainkey == $varname ) {
+			$exactmatch = ($mainkey == $varname);
+			$pluralmatch = ($mainkey . 's' == $varname);
+			if( $exactmatch || ($check_multiples  && $pluralmatch) ) {
 				if( $this->mainkeys[$mainkey] == 1 ) {
 					if( $this->subkeys[$mainkey] == false ) {
-						return $row['Value'];
+						if($check_multiples && !$exactmatch) {
+							$rtrn = array();
+							$rtrn[] = $row['Value'];
+							return $rtrn;
+						} else {
+							return $row['Value'];
+						}
 					} else {
 						$subrows = array();
 						foreach( $this->rows as $rr ) {
 							if( $row['ProjectInfoID'] == $rr['ProjectInfoID']) {
 								$subrows[] = $rr;
-
 							}
 						}
-						return new ProjectInfoValues( $this, $subrows );
+						if($check_multiples && !$exactmatch) {
+							$rtrn = array();
+							$rtrn[] = new ProjectInfoValues( $this, $subrows );
+							return $rtrn;
+						} else {
+							return new ProjectInfoValues( $this, $subrows );
+						}
 					}
 				} else {
 					if( $this->subkeys[$mainkey] == false ) {
@@ -68,7 +84,6 @@ class ProjectInfoData
 						foreach( $this->rows as $rr ) {
 							if( $rr['MainKey'] == $mainkey) {
 								$result[] = $rr['Value'];
-								
 							}
 						}
 						return $result;
@@ -78,7 +93,6 @@ class ProjectInfoData
 							if( $rr['MainKey'] == $mainkey) {
 								$subrows = array();
 								foreach( $this->rows as $rrr ) {
-
 									if( $rr['ProjectInfoID'] == $rrr['ProjectInfoID']) {
 										$subrows[] = $rrr;
 									}
@@ -93,24 +107,30 @@ class ProjectInfoData
 		}
 		return null;
 	}
+
+	function count() {
+		return count($this->mainkeys);
+	}
 }
 
-class ProjectInfoValues {
+class ProjectInfoValues implements Countable {
 	private $rows;
-		function ProjectInfoValues( $projectinfo, $subrows ) {
-			$this->rows = $subrows;
-		}
-		function __get( $varname )
-		{
-			foreach( $this->rows as $row ) {
-				$subkey = $row['SubKey'];
-				if( $subkey == $varname ) {
-					return $row['Value'];
-				}
+	function ProjectInfoValues( $projectinfo, $subrows ) {
+		$this->rows = $subrows;
+	}
+	function __get( $varname )
+	{
+		foreach( $this->rows as $row ) {
+			$subkey = $row['SubKey'];
+			if( $subkey == $varname ) {
+				return $row['Value'];
 			}
-			return null;
 		}
+		return null;
+	}
+
+	function count() {
+		return count($this->rows);
+	}
 }
-
-
 ?>
