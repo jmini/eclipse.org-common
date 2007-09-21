9fc2d83e1456aa7d93a55585b244ac07794235f6
ngervais <ngervais>
2007-09-21 19:17:06 +0000

    Changed NewsParse and MailParse for the new ProjectInfo Database

--- system/project_details_parsing.php
+++ system/project_details_parsing.php
@@ -55,87 +55,77 @@ function GetFile( $name, $filename, $docroot ) {
 
 /***************************************
  * Name: NewsParse
- * function: Parses the news file and build the project specific html
- * I/O: takes the name of the project(getProjectID()) and a pointer to the html output stream, as
- * well as a bool to turn off the descriptive text
+ * function: Parses the newsgroup array object and output specific html
  * 
  * By: M. Ward
  * Date: Dec 13/05
+ * 
+ * Modified By: N. Gervais
+ * Modified On : Sep 21/07
+ * ChangeLog : Changed this to use the new project info database objects
 ****************************************/
-function NewsParse( $name, &$html, $id ) {
-  $group_file = GetFile( $name, "newsgroup",$_SERVER['DOCUMENT_ROOT'] );
-  if( !file_exists($group_file) ) {
-  	$group_file = GetFile( $name, "newsgroup" , $_SERVER['DOCUMENT_ROOT'] . "/projects/temporary" );  
-    if( !file_exists($group_file) )
-      return;
-  }
-  //get the contents
-  $contents = file_get_contents($group_file);
-  //now break them down        
-  $array = explode("::",$contents);
-  $group_count = count($array);
-  for ( $loop = 1; $loop < $group_count; $loop+=2) {
-    $news_name = $array[$loop];
-    $news_html = "<a href=\"news://news.eclipse.org/" . $news_name . "\" alt='News server' title=\"News server\"/>$news_name</a>";
-    $webnews_html = "<a href=\"http://www.eclipse.org/newsportal/thread.php?group=" . $news_name . "\""  . "><img src='images/discovery.gif' alt='Web interface' title=\"Web interface\" /></a>";
-	$newsarch_html = "<a href=\"http://dev.eclipse.org/newslists/news." . $news_name . "/\""  . " alt='Archive' title=\"Archive\" />Newsgroup Archive</a>";
-	$newsrss_html = "<a href=\"http://dev.eclipse.org/newslists/news." . $news_name . "/maillist.rss\">RSS Feed  </a><a href=\"http://dev.eclipse.org/newslists/news." . $news_name . "/maillist.rss\"><img src='/eclipse.org-common/themes/Phoenix/images/rss_btn.gif' alt='RSS Feed' title=\"RSS Feed\" /></a>";
-	$newssearch = "<FORM NAME=\"$news_name\" METHOD=GET ACTION=\"/search/search.cgi\" onsubmit=\"fnSetAction();\">
-			<table bgcolor=#EEEEEE border=0>
-  			<TR><TD>Search $news_name 
-      			<INPUT TYPE=\"text\" NAME=\"q\" value=\"\" SIZE=\"18\" class=\"groupsearch\">
-      			<INPUT TYPE=\"submit\" NAME=\"cmd\" value=\"Search\" class=\"groupsearch\">
-      			<INPUT TYPE=\"hidden\" NAME=\"form\" value=\"extended\">
-      	        <input type=\"hidden\" name=\"wf\" value=\"574a74\">
-      	        <INPUT TYPE=\"hidden\" NAME=\"ul\" value=\"/newslists/news.$news_name\">
-      	        <INPUT TYPE=\"hidden\" NAME=\"t\" value=\"5\">
-      	        <INPUT TYPE=\"hidden\" NAME=\"t\" value=\"News\">
-      	        <INPUT TYPE=\"hidden\" NAME=\"t\" value=\"Mail\"></td></tr></Table></FORM>";
-	$description = $array[$loop+1];
-	$html .= "<p><a href=\"javascript:switchMenu('$news_name.$id');\" title=\"Description\"><img src='images/plus.gif' alt='Description' title=\"Description\"></a>  $news_html $webnews_html </p><div id=\"$news_name.$id\" class=\"switchcontent\"> <p>$description </p> <p>$newsarch_html</p> <p>$newsrss_html</p> <p>$newssearch</p></div>";
-  }          
+function NewsParse( $newsgroupArray ) {
+
+	if (count($newsgroupArray)){
+	    $news_name = trim($newsgroupArray->name);
+	    if ($news_name != ""){
+	    $news_html = "<a href=\"news://news.eclipse.org/" . $news_name . "\" alt='News server' title=\"News server\"/>$news_name</a>";
+	    $webnews_html = "<a href=\"http://www.eclipse.org/newsportal/thread.php?group=" . $news_name . "\""  . "><img src='images/discovery.gif' alt='Web interface' title=\"Web interface\" /></a>";
+		$newsarch_html = "<a href=\"http://dev.eclipse.org/newslists/news." . $news_name . "/\""  . " alt='Archive' title=\"Archive\" />Newsgroup Archive</a>";
+		$newsrss_html = "<a href=\"http://dev.eclipse.org/newslists/news." . $news_name . "/maillist.rss\">RSS Feed  </a><a href=\"http://dev.eclipse.org/newslists/news." . $news_name . "/maillist.rss\"><img src='/eclipse.org-common/themes/Phoenix/images/rss_btn.gif' alt='RSS Feed' title=\"RSS Feed\" /></a>";
+		$newssearch = "<FORM NAME=\"$news_name\" METHOD=GET ACTION=\"/search/search.cgi\" onsubmit=\"fnSetAction();\">
+				<table bgcolor=#EEEEEE border=0>
+	  			<TR><TD>Search $news_name 
+	      			<INPUT TYPE=\"text\" NAME=\"q\" value=\"\" SIZE=\"18\" class=\"groupsearch\">
+	      			<INPUT TYPE=\"submit\" NAME=\"cmd\" value=\"Search\" class=\"groupsearch\">
+	      			<INPUT TYPE=\"hidden\" NAME=\"form\" value=\"extended\">
+	      	        <input type=\"hidden\" name=\"wf\" value=\"574a74\">
+	      	        <INPUT TYPE=\"hidden\" NAME=\"ul\" value=\"/newslists/news.$news_name\">
+	      	        <INPUT TYPE=\"hidden\" NAME=\"t\" value=\"5\">
+	      	        <INPUT TYPE=\"hidden\" NAME=\"t\" value=\"News\">
+	      	        <INPUT TYPE=\"hidden\" NAME=\"t\" value=\"Mail\"></td></tr></Table></FORM>";
+		$description = $newsgroupArray->description;
+		$html = "<p><a href=\"javascript:switchMenu('$news_name.$id');\" title=\"Description\"><img src='images/plus.gif' alt='Description' title=\"Description\"></a>  $news_html $webnews_html </p><div id=\"$news_name.$id\" class=\"switchcontent\"> <p>$description </p> <p>$newsarch_html</p> <p>$newsrss_html</p> <p>$newssearch</p></div>";
+	    }
+	}     
+	return $html;     
 }
 
 /***************************************
  * Name: MailParse
- * function: Parses the mail file and build the project specific html
- * I/O: takes the name of the project(getProjectID()) and a pointer to the html output stream
+ * function: Parses the mail array object and output specific html
  * 
  * By: M. Ward
  * Date: Dec 21/05
+ * 
+ * Modified By: N. Gervais
+ * Modified On : Sep 21/07
+ * ChangeLog : Changed this to use the new project info database objects * 
 ****************************************/
-function MailParse( $name, &$html, $id ) {
-  $group_file = GetFile( $name, "maillist",$_SERVER['DOCUMENT_ROOT'] );
-  if( !file_exists($group_file) ) {
-  	$group_file = GetFile( $name, "maillist", $_SERVER['DOCUMENT_ROOT'] . "/projects/temporary");
-    if( !file_exists($group_file) )
-      return;
+function MailParse( $mailingListArray ) {
+	
+if (count($mailingListArray)) {
+	$mail_name = trim($mailingListArray->name);
+    if ($mail_name != ""){
+	    $mail_html = "<a href=\"http://dev.eclipse.org/mailman/listinfo/" . $mail_name . "\" alt=\"Subscribe\" title=\"Subscribe\" />$mail_name</a>";
+		$mailarch_html = "<a href=\"http://dev.eclipse.org/mhonarc/lists" . $mail_name . "/\""  . "alt='Archive' title=\"Archive\" />Mailing list archive</a>";
+		$mailrss_html = "<a href=\"http://dev.eclipse.org/mhonarc/lists." . $mail_name . "/maillist.rss\">RSS Feed  </a><a href=\"http://dev.eclipse.org/mhonarc/lists/" . $mail_name . "/maillist.rss\" ><img src='/eclipse.org-common/themes/Phoenix/images/rss_btn.gif' alt='RSS Feed' title=\"RSS Feed\" /></a>";
+		$mailsearch = "<FORM name=\"$mail_name\" METHOD=GET ACTION=\"/search/search.cgi\" onsubmit=\"fnSetAction();\">
+				<table bgcolor=#EEEEEE border=0>
+	  			<TR><TD>Search $mail_name 
+	      			<INPUT TYPE=\"text\" NAME=\"q\" value=\"\" SIZE=\"18\" class=\"groupsearch\">
+	      			<INPUT TYPE=\"submit\" NAME=\"cmd\" value=\"Search\" class=\"groupsearch\">
+	      			<INPUT TYPE=\"hidden\" NAME=\"form\" value=\"extended\">
+	      	        <input type=\"hidden\" name=\"wf\" value=\"574a74\">
+	      	        <INPUT TYPE=\"hidden\" NAME=\"ul\" value=\"/mhonarc/lists/$mail_name\">
+	      	        <INPUT TYPE=\"hidden\" NAME=\"t\" value=\"5\">
+	      	        <INPUT TYPE=\"hidden\" NAME=\"t\" value=\"News\">
+	      	        <INPUT TYPE=\"hidden\" NAME=\"t\" value=\"Mail\"></td></tr></Table></FORM>";
+		$description = $mailingListArray->description;
+	$html = "<p><a href=\"javascript:switchMenu('$mail_name.$id');\" title=\"Description\"><img src='images/plus.gif' alt='Description' title=\"Description\"></a> $mail_html </p> <div id=\"$mail_name.$id\" class=\"switchcontent\"> <p> $description </p><p>$mailarch_html</p><p>$mailrss_html</p><p>$mailsearch</p></div>";
+    }
   }
-  //get the contents
-  $contents = file_get_contents($group_file);
-  //now break them down        
-  $array = explode("::",$contents);
-
-  $group_count = count($array);
-  for ( $loop = 1; $loop < $group_count; $loop+=2) {
-    $mail_name = $array[$loop];
-    $mail_html = "<a href=\"http://dev.eclipse.org/mailman/listinfo/" . $mail_name . "\" alt=\"Subscribe\" title=\"Subscribe\" />$mail_name</a>";
-	$mailarch_html = "<a href=\"http://dev.eclipse.org/mhonarc/lists" . $mail_name . "/\""  . "alt='Archive' title=\"Archive\" />Mailing list archive</a>";
-	$mailrss_html = "<a href=\"http://dev.eclipse.org/mhonarc/lists." . $mail_name . "/maillist.rss\">RSS Feed  </a><a href=\"http://dev.eclipse.org/mhonarc/lists/" . $mail_name . "/maillist.rss\" ><img src='/eclipse.org-common/themes/Phoenix/images/rss_btn.gif' alt='RSS Feed' title=\"RSS Feed\" /></a>";
-	$mailsearch = "<FORM name=\"$mail_name\" METHOD=GET ACTION=\"/search/search.cgi\" onsubmit=\"fnSetAction();\">
-			<table bgcolor=#EEEEEE border=0>
-  			<TR><TD>Search $mail_name 
-      			<INPUT TYPE=\"text\" NAME=\"q\" value=\"\" SIZE=\"18\" class=\"groupsearch\">
-      			<INPUT TYPE=\"submit\" NAME=\"cmd\" value=\"Search\" class=\"groupsearch\">
-      			<INPUT TYPE=\"hidden\" NAME=\"form\" value=\"extended\">
-      	        <input type=\"hidden\" name=\"wf\" value=\"574a74\">
-      	        <INPUT TYPE=\"hidden\" NAME=\"ul\" value=\"/mhonarc/lists/$mail_name\">
-      	        <INPUT TYPE=\"hidden\" NAME=\"t\" value=\"5\">
-      	        <INPUT TYPE=\"hidden\" NAME=\"t\" value=\"News\">
-      	        <INPUT TYPE=\"hidden\" NAME=\"t\" value=\"Mail\"></td></tr></Table></FORM>";
-	$description = $array[$loop+1];
-	$html .= "<p><a href=\"javascript:switchMenu('$mail_name.$id');\" title=\"Description\"><img src='images/plus.gif' alt='Description' title=\"Description\"></a> $mail_html </p> <div id=\"$mail_name.$id\" class=\"switchcontent\"> <p> $description </p><p>$mailarch_html</p><p>$mailrss_html</p><p>$mailsearch</p></div>";
-  }          
+  return $html;      
 }
 
 
