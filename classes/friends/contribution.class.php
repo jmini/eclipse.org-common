df90bc7c60fce00fb392f6a283aa07d94ca33bad
ngervais <ngervais>
2007-11-20 18:55:55 +0000

    Contributions Added

---
+++ classes/friends/contribution.class.php
@@ -0,0 +1,150 @@
+<?php
+/*******************************************************************************
+ * Copyright (c) 2007 Eclipse Foundation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Nathan Gervais (Eclipse Foundation)- initial API and implementation
+ *******************************************************************************/
+require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/smartconnection.class.php");
+require_once("/home/data/httpd/eclipse-php-classes/system/dbconnection_rw.class.php");
+
+class Contribution {
+	
+	private $friend_id = 0;
+	private $contribution_id = 0;
+	private $date_expired = 0;
+	private $amount = 0;
+	private $message = 0;
+	private $transaction_id = 0;
+	
+	function getFriendID(){
+		return $this->friend_id;
+	}
+	function getContributionID(){
+		return $this->contribution_id;
+	}
+	function getDateExpired(){
+		return $this->date_expired;
+	}
+	function getAmount(){
+		return $this->amount;
+	}
+	function getMessage(){
+		return $this->message;
+	}
+	function getTransactionID(){
+		return $this->transaction_id;
+	}
+	
+	function setFriendID($_friend_id){
+		$this->friend_id = $_friend_id;
+	}
+	function setContributionID($_contribution_id){
+		$this->contribution_id = $_contribution_id;
+	}
+	function setDateExpired($_date_expired){
+		$this->date_expired = $_date_expired;
+	}
+	function setAmount($_amount){
+		$this->amount = $_amount;
+	}
+	function setMessage($_message){
+		$this->message = $_message;
+	}
+	function setTransactionID($_transaction_id){
+		$this->transaction_id = $_transaction_id;
+	}
+	
+	function insertContribution(){
+		$result = 0;
+		$App = new App();
+		$dbc = new DBConnectionRW();
+		$dbh = $dbc->connect();
+		
+		if (selectContributionExists($this->getTransactionID())){
+			$result = -1;
+		}
+		else
+		{
+			# insert
+			$sql = "INSERT INTO friends_contributons (
+					friend_id,
+					contribution_id,
+					date_expired,
+					amount,
+					message,
+					transaction_id)
+					VALUES (
+					" . $App->returnQuotedString($this->getFriendID()) . ",
+					" . $App->returnQuotedString($this->getContributionID()) . ",
+					" . $App->returnQuotedString($this->getDateExpired()) . ",
+					" . $App->returnQuotedString($this->getAmount() . ",
+					" . $App->returnQuotedString($this->getMessage()) . ",
+					" . $App->returnQuotedString($this->getTransactionID()) . ")";
+			mysql_query($sql, $dbh);
+		}
+
+		$dbc->disconnect();
+		return $result;
+	}
+	
+	function selectContributionExists($_transaction_id){
+		$result = 0;
+		if ($_transaction_id != "")
+		{
+			$App = new App();
+
+			$dbc = new DBConnectionRW();
+			$dbh = $dbc->connect();
+
+			$sql = "SELECT transactionID
+					FROM friends_contributions
+					WHERE transaction_id = " . $App->returnQuotedString($_transaction_id);
+
+			$result = mysql_query($sql, $dbh);
+			$myrow = mysql_fetch_array($result);
+
+			$result = $myrow['RecordCount'] > 1 ? 1 : 0;
+
+			$dbc->disconnect();
+
+		}
+		return $result;			
+		}
+	}
+	
+	function selectContribution($_contribution_id)
+	{
+		if($_contribution_id != "")  {
+			$App = new App();
+
+			$dbc = new DBConnectionRW();
+			$dbh = $dbc->connect();
+
+			$sql = "SELECT friend_id,
+							contribution_id,
+							date_expired,
+							amount,
+							message,
+							transaction,
+					FROM friends_contributions 
+					WHERE contribution_id = " . $App->returnQuotedString($_contribution_id);
+
+			$result = mysql_query($sql, $dbh);
+
+			if ($myrow = mysql_fetch_array($result))	{
+				$this->setFriendID			($myrow["friend_id"]);
+				$this->setContributionID	($myrow["contribution_id"]);
+				$this->setDateExpired		($myrow["date_expired"]);
+				$this->setAmount			($myrow["amount"]);
+				$this->setMessage			($myrow["message"]);
+				$this->setTransactionID		($myrow["transaction_id"]);
+			}
+			$dbc->disconnect();
+		}
+	}
+}
---
+++ classes/friends/friendsContributions.class.php
@@ -0,0 +1,57 @@
+<?php
+/*******************************************************************************
+ * Copyright (c) 2007 Eclipse Foundation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Nathan Gervais (Eclipse Foundation)- initial API and implementation
+ *******************************************************************************/
+require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/smartconnection.class.php");
+require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/classes/friends/contribution.class.php");
+require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/classes/friends/friend.class.php");
+
+class FriendsContributions {
+	private $friend_id;
+	private $contribution_id;
+	private $friendObject;
+	private $contributionObject;
+	
+	
+	# default constructor
+	function FriendsContributions() {
+		$this->friend_id 		= "";
+		$this->contribution_id	= "";
+		$this->friendObject	= new Friend();
+		$this->contribitionObject	= new Contribution();
+	}
+	
+	function getFriendID() {
+		return $this->friend_id;
+	}
+	function getContribution() {
+		return $this->contribution_id;
+	}
+	function getFriendObject() {
+		return $this->friendObject;
+	}
+	function getCategoryObject() {
+		return $this->contributionObject;
+	}
+	
+	function setFriendID($_friend_id){
+		$this->friend_id = $_friend_id;
+	}
+	function setContributionID($_contribution_id) {
+		$this->contribution_id = $_contribution_id;
+	}
+	function setFriendObject($_friendObject) {
+		$this->friendObject = $_friendObject;
+	}
+	function setContributionObject($_contributionObject){
+		$this->contributionObject = $_contributionObject;
+	}
+	
+}
