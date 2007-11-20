acb363302b3e5c5e94eb5a9871437ffc1c3b1d32
droy <droy>
2007-11-20 16:35:39 +0000

    *** empty log message ***

--- system/session.class.php
+++ system/session.class.php
@@ -32,7 +32,7 @@ class Session {
 	 */
 	function Session($persistent=null) {
 		$this->persistent = $persistent;
-		session_set_cookie_params(0, "/", "eclipse.org", false, true);
+		session_set_cookie_params(0, "/", ".eclipse.org", false, true);
 		session_start();
 		
 		if(isset($_SESSION['Friend'])) {
