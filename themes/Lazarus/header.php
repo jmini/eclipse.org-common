d6e5662eddd71b21b0ff0b63971d19e3e807a2b4
droy <droy>
2008-01-23 19:40:59 +0000

    [210887] UTF-8 incorrect in meta tag

--- themes/Lazarus/header.php
+++ themes/Lazarus/header.php
@@ -19,11 +19,9 @@
 		$www_prefix = $App->getWWWPrefix();
 	}
 ?>
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head><title><?= $pageTitle ?></title><meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<meta name="author" content="<?= $pageAuthor ?>" />
+<head><title><?= $pageTitle ?></title><meta name="author" content="<?= $pageAuthor ?>" />
 <meta name="keywords" content="<?= $pageKeywords ?>" />
 <link rel="stylesheet" type="text/css" href="/eclipse.org-common/themes/Lazarus/css/small.css" title="small" />
 <link rel="alternate stylesheet" type="text/css" href="/eclipse.org-common/themes/Lazarus/css/large.css" title="large" />
@@ -33,13 +31,9 @@
 <script type="text/javascript" src="/eclipse.org-common/themes/Phoenix/styleswitcher.js"></script>
 	<?php if( isset($extraHtmlHeaders) ) echo $extraHtmlHeaders; ?>
 </head>
-
 <body>
 <div id="header">
 </div>
-
 <div id="headerlogo">
-
 	<a href="/"><img src="/eclipse.org-common/themes/Lazarus/images/header_logo.gif" alt="Eclipse Logo" class="logo" border="0" height="75" width="241" /></a>
-
 </div>
--- themes/Miasma/header.php
+++ themes/Miasma/header.php
@@ -21,7 +21,6 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head><title><?= $pageTitle ?></title>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<meta name="author" content="<?= $pageAuthor ?>" />
 	<meta name="keywords" content="<?= $pageKeywords ?>" />
 	<link rel="stylesheet" type="text/css" href="/eclipse.org-common/themes/Miasma/css/visual.css" media="screen" />
@@ -29,7 +28,6 @@
 	<link rel="stylesheet" type="text/css" href="/eclipse.org-common/themes/Phoenix/css/print.css" media="print" />
 	<?php if( isset($extraHtmlHeaders) ) echo $extraHtmlHeaders; ?>
 </head>
-
 <body>
 <div id="header">
 	<a href="/"><img src="/eclipse.org-common/themes/Miasma/images/header_logo.gif" alt="Eclipse Logo" class="logo" border="0" height="71" width="128" /></a>
