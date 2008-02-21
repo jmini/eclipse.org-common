4312d937e06a28f66e5c21cd78caaaf41ba5e34a
droy <droy>
2008-02-21 16:05:11 +0000

    Swicth google code to $App

--- themes/Miasma/header.php
+++ themes/Miasma/header.php
@@ -31,13 +31,7 @@
 <body>
 <div id="header">
 	<a href="/"><img src="/eclipse.org-common/themes/Miasma/images/header_logo.gif" alt="Eclipse Logo" class="logo" border="0" height="71" width="128" /></a>
-	<div id="searchbar">
-<form action="http://www.google.com/cse" id="searchbox_001774807050229944597:xrjquhh7kg8">
-  <input type="hidden" name="cx" value="001774807050229944597:xrjquhh7kg8" />
-  <input class="input" type="text" name="q" size="60" />
-  <input class="button" type="button" name="sa" value="Search" onclick="this.submit();" alt="Search" title="Search" />
-</form>
-<script type="text/javascript" src="http://www.google.com/coop/cse/brand?form=searchbox_001774807050229944597%3Axrjquhh7kg8&lang=en"></script>
+	<div id="searchbar"><?= $App->getGoogleSearchHTML() ?>
 	</div>
 	<ul id="headernav">
 		<li class="first"><a href="<?= $www_prefix ?>/org/foundation/contact.php">Contact</a></li>
--- themes/Phoenix/menu.php
+++ themes/Phoenix/menu.php
@@ -41,12 +41,7 @@
 		</ul>
 	</div>
 	<div id="header-utils">
-<form action="http://www.google.com/cse" id="searchbox_001774807050229944597:xrjquhh7kg8">
-  <input type="hidden" name="cx" value="001774807050229944597:xrjquhh7kg8" />
-  <input class="input" type="text" name="q" size="60" />
-  <input class="button" type="image" name="sa" value="Search" onclick="this.submit();" alt="Search" title="Search" src="/eclipse.org-common/themes/Phoenix/images/search_btn.gif"/>
-</form>
-<script type="text/javascript" src="http://www.google.com/coop/cse/brand?form=searchbox_001774807050229944597%3Axrjquhh7kg8&lang=en"></script>
+<?= $App->getGoogleSearchHTML() ?>
 		<ul>
 			<?php
 				if($pageRSS != "") {
