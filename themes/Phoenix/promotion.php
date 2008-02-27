ae8f7c45cd2a82e3cad406894b13d99a9fba607b
ngervais <ngervais>
2008-02-27 18:48:20 +0000

    *** empty log message ***

--- themes/Phoenix/promotion.php
+++ themes/Phoenix/promotion.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" type="text/css" href="/home/layout.css" media="screen" />	
 <div id="econHeader" class="eclipseConHeader">
 	<div style="width:616px;float:left;">
-		<a href="http://www.eclipsecon.org/2008/index.php?page=registration/"><img src="/home/images/eclipseconDownload.gif"></a>
+		<a href="http://www.eclipsecon.org/2008/index.php?page=registration/"><img src="/home/images/eclipsecon.gif"></a>
 	</div>
 	<div class="registerNow">
 		<span class="econDate">March 17th - 20th 2008<br/>Santa Clara, CA</span> <br/>
