cb18c9e6f89a5ccb4588bca593c22250674a66e5
droy <droy>
2008-05-22 18:14:20 +0000

    *** empty log message ***

--- themes/Phoenix/header.php
+++ themes/Phoenix/header.php
@@ -23,6 +23,7 @@
 <head><title><?= $pageTitle ?></title><meta name="author" content="<?= $pageAuthor ?>" />
 <meta name="keywords" content="<?= $pageKeywords ?>" /><link rel="stylesheet" type="text/css" href="/eclipse.org-common/themes/Phoenix/css/small.css" title="small" /><link rel="alternate stylesheet" type="text/css" href="/eclipse.org-common/themes/Phoenix/css/large.css" title="large" /><link rel="stylesheet" type="text/css" href="/eclipse.org-common/themes/Phoenix/css/visual.css" media="screen" /><link rel="stylesheet" type="text/css" href="/eclipse.org-common/themes/Phoenix/css/layout.css" media="screen" />
 <!--[if IE]> 	<link rel="stylesheet" type="text/css" href="/eclipse.org-common/themes/Phoenix/css/ie_style.css" media="screen"/> <![endif]-->
+<!--[if IE 6]> 	<link rel="stylesheet" type="text/css" href="/eclipse.org-common/themes/Phoenix/css/ie6_style.css" media="screen"/> <![endif]-->
 <link rel="stylesheet" type="text/css" href="/eclipse.org-common/themes/Phoenix/css/print.css" media="print" />
 <link rel="stylesheet" type="text/css" href="/eclipse.org-common/themes/Phoenix/css/header.css" media="screen" />
 <script type="text/javascript" src="/eclipse.org-common/themes/Phoenix/styleswitcher.js"></script>
