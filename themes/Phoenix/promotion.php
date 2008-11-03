9c0ba44674a7f90c90d30ed26342eefa5603efd9
droy <droy>
2008-11-03 19:47:29 +0000

    *** empty log message ***

--- themes/Phoenix/promotion.php
+++ themes/Phoenix/promotion.php
@@ -1,10 +1,10 @@
 <link rel="stylesheet" type="text/css" href="/home/layout.css" media="screen" />	
 <div id="econHeader" class="eclipseConHeader">
 	<div style="float:left;">
-		<a href="http://www.eclipsecon.org/summiteurope2008/registration"><img src="/home/images/eseBanner.png"></a>
+		<a href="http://eclipse.org/go/ESE_Banner@TOP"><img src="/home/images/eseBanner.png"></a>
 	</div>
 	<div class="registerNow">
 		<span class="econDate">November 19th - 20th 2008<br/>Ludwigsburg, Germany</span> <br/>
-		<a href="http://www.eclipsecon.org/summiteurope2008/registration"><img src="/home/images/registerNowBlue.png"></a>
+		<a href="http://eclipse.org/go/ESE_Banner@TOP"><img src="/home/images/registerNowBlue.png"></a>
 	</div>
 </div>	
