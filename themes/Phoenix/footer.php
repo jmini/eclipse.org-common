ba7cd77d53100b5ad029b5a27b257fe2a9b7898f
droy <droy>
2008-11-12 21:55:51 +0000

    *** empty log message ***

--- themes/Phoenix/footer.php
+++ themes/Phoenix/footer.php
@@ -14,7 +14,7 @@
 <ul id="footernav">
 <li class="first"><a href="<?= $App->getWWWPrefix() ?>/">Home</a></li>
 <li><a href="<?= $App->getWWWPrefix() ?>/legal/privacy.php">Privacy Policy</a></li>
-<li><a href="<?= $App->getWWWPrefix() ?>/legal/termsofuse.php">Terms of Use</a><img src="/images/updated.png" align="absmiddle" /></li>
+<li><a href="<?= $App->getWWWPrefix() ?>/legal/termsofuse.php">Terms of Use</a><a href="/org/press-release/20081112_termsofuse.php"><img src="/images/updated.png" align="absmiddle" /></a></li>
 </ul>
 <p>Copyright &copy; <?=date("Y");?> The Eclipse Foundation. All Rights Reserved</p>
 </div></body></html>
