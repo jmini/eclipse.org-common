68bf6968d302d9bb4a1aa71cda8aa4f97f2b4b11
droy <droy>
2008-12-16 19:12:54 +0000

    *** empty log message ***

---
+++ themes/Phoenix/promotion.php
@@ -0,0 +1,11 @@
+<? if (date("Y") < 2009) { ?>
+<link rel="stylesheet" type="text/css" href="/home/layout.css" media="screen" />	
+<div id="econHeader" class="eclipseConHeader">
+	<div style="float:left;">
+		<a href="http://eclipse.org/go/EC_BANNER@TOP1"><img src="/images/econ09/left.jpg"></a>
+	</div>
+	<div class="registerNow">
+		<a href="http://eclipse.org/go/EC_BANNER@TOP1"><img src="/images/econ09/right.png"></a>
+	</div>
+</div>	
+<? } ?>
