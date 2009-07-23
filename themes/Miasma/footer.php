623ac174c4204303983f50ca9b283482368b3911
droy <droy>
2009-07-23 19:14:26 +0000

    *** empty log message ***

--- themes/Miasma/footer.php
+++ themes/Miasma/footer.php
@@ -2,7 +2,7 @@
 	<ul id="footernav">
 		<li class="first"><a href="/">Home</a></li>
 		<li><a href="<?= $www_prefix ?>/legal/privacy.php">Privacy Policy</a></li>
-		<li><a href="<?= $www_prefix ?>/legal/termsofuse.php">Terms of Use</a><a href="/org/press-release/20081112_termsofuse.php"><img src="/images/updated.png" align="absmiddle" /></a></li>
+		<li><a href="<?= $www_prefix ?>/legal/termsofuse.php">Terms of Use</a></li>
 	</ul>
 	<p>Copyright &copy; <?=date("Y");?> The Eclipse Foundation. All Rights
 Reserved</p>
